a13fc9dff73dafe6a0ee54954c377e92a111656c
boygeniusnr1 <boygeniusnr1@gmail.com>
2025-05-28 09:17:25 +1200

    Navbar + hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import pageInsight from 'astro-page-insight';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [pageInsight()]
+});
