a2dcae37d283728b2ec548ca43dd980a08c563e7
boygeniusnr1 <boygeniusnr1@gmail.com>
2025-06-28 12:16:48 +1200

    feat: enhance event descriptions and improve layout structure for better user experience

--- src/data/swaps.js
+++ src/data/swaps.js
@@ -9,14 +9,23 @@ export const swapEventsDetails = [
     address: "69 Muritai St, Tāhunanui, Nelson",
     description: `
       <p>
-        Join us at Tāhunanui School for a fun and eco-friendly clothing swap event!
-        This is a fantastic opportunity for students, parents, and local community
-        members to refresh their wardrobes sustainably. Bring your gently used clothes
-        and discover new-to-you treasures from others. It's a perfect way to promote
-        circular fashion and reduce textile waste in our community.
+        A big Thank you to Tahunanui School for letting us use their Muritai center for our first swapping event!<br>
+        We are proud and excited to make this event a big success, and create a fantastic opportunity for students, parents, and local community
+        members to refresh their wardrobes sustainably. 
+        To turn this event into a success, we need your help!
+        The quality and amount of clothing brought in will determine what you can choose from. 
+        So please bring in stuff just as nice as the stuff you hope to bring home with you from the swap.
+        Happy faces, kindness and and some patience will contribute to a nice and relaxed atmosphere.
+        <br><br>
+        
+        We hope everyone will leave with a smile on their face and a bag full of new-to-you clothes.
+        However, we can not gurantee that you take home as many items as you brought in. Since we don't know how many people will attend, and how many items of what size they will bring in, we can not guarantee that there will be something of their liking for everyone to take home.
+        In that case, we hope the experience of the rewarding feeling of passing on your favourite piece of clothing to be enjoyed by another person in your community is stead of having to send it off to landfill, will make up for it. 
 
         Note: This event will be mainly focused on children's clothing, but all ages are welcome to participate.
-        
+        <br><br>
+
+
       </p>
       <p>
         Come and be part of the Clothes Swap Society!
